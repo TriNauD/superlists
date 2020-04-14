03eb490fa2a74fed142624ac2803fe2baae6088a
TriNauD <17801202079@163.com>
2020-04-14 10:15:31 +0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -11,11 +11,13 @@ class HomePageTest(TestCase):
 		self.assertEqual(found.func,home_page) 
 
 	def test_home_page_returns_correct_html(self):
-		request = HomePageTest() #1
-		response = home_page(request) #2
-		html = response.content.decode('utf8') #3
-		self.assertTrue(html.startswith('<html>')) #4
-		self.assertIn('<title>To-Do lists</title>',html) #5
-		self.assertTrue(html.endswith('</html>')) #4
+		response = self.client.get('/') #1
+
+		html = response.content.decode('utf8') #2
+		self.assertTrue(html.startswith('<html>')) 
+		self.assertIn('<title>To-Do lists</title>',html) 
+		self.assertTrue(html.strip().endswith('</html>'))
+
+		self.assertTemplateUsed(response,'home.html')#3
 		
 # Create your tests here.
